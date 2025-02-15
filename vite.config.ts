a9c81c6f02275afc2a5e9114daed0bb7806be23f
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-16 04:27:45 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Netlify expects this by default
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
