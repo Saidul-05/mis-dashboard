e99560bed7f8ba914fbd502de60f4b8393108470
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-16 03:41:07 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+// src/App.tsx
 import React from 'react';
+import MISSystem from './components/MISSystem/MISSystem'; // Correct path
 import { AppProvider } from './contexts/AppContext';
 import { WebSocketProvider } from './contexts/WebsocketContext';
-import MISSystem from './components/MISSystem';
 import ErrorBoundary from './components/common/ErrorBoundary';
 
 const App: React.FC = () => {
