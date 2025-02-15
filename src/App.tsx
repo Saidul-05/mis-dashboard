f4a8ed21fd22d1d711bfc6e16f247a62bdf947a9
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-16 04:05:42 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // src/App.tsx
 import React from 'react';
-import MISSystem from './components/MISSystem/MISSystem'; // Correct path
 import { AppProvider } from './contexts/AppContext';
 import { WebSocketProvider } from './contexts/WebsocketContext';
 import ErrorBoundary from './components/common/ErrorBoundary';
+import MISSystem from './components/MISSystem/MISSystem';
 
 const App: React.FC = () => {
   return (
     <ErrorBoundary>
       <WebSocketProvider>
         <AppProvider>
-          <MISSystem />
+          <div className="h-screen w-screen">
+            <MISSystem />
+          </div>
         </AppProvider>
       </WebSocketProvider>
     </ErrorBoundary>
