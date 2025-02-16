5deefa7e7f53fadeb62a431d9bf29c242616cc64
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-17 04:06:42 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  },
-  server: {
-    proxy: {
-      '/ws': {
-        target: 'ws://localhost:3001',
-        ws: true
-      }
-    }
   }
 });
