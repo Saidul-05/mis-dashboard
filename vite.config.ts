1192015e7054bbb774a881cc34c256be19fc3108
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-17 05:08:23 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './public/index.html' 👈 Explicit entry point
-      }
-    }
+    emptyOutDir: true
+  },
+  server: {
+    host: true,
+    port: 3000
   }
-})
+});
