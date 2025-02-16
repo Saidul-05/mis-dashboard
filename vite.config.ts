fe660f73b30d0b870ddef88097394d0b59a24cdf
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-17 04:58:41 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    rollupOptions: {
+      input: {
+        main: './public/index.html' 👈 Explicit entry point
+      }
+    }
   }
-});
+})
